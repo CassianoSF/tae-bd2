9d37783859fb878c189fbd484ea978b9eb08c400
CassianoSF <cassianosfranco@gmail.com>
2019-12-05 10:03:15 -0300

    trim

--- sql/ex3.sql
+++ sql/ex3.sql
@@ -1,25 +1,31 @@
 
 -- 3.
 -- a) Buscar o nome e o ano dos filmes de ação;
-SELECT titulo, ano FROM Filmes
+SELECT TRIM(titulo), ano FROM Filmes
 JOIN Genero_Filme ON Genero_Filme.filme_id = Filmes.id
 JOIN Generos ON Genero_Filme.genero_id = Generos.id
 WHERE Generos.descricao = 'Ação';
 
 -- b) Buscar os filmes em que a pessoa ‘Marlon Brando’ possui créditos como ator;
-SELECT titulo FROM Filmes
+SELECT TRIM(titulo) FROM Filmes
 JOIN Creditos ON Creditos.filme_id = Filmes.id
 JOIN Pessoas  ON Creditos.pessoa_id = Pessoas.id
 JOIN Tipos    ON Creditos.tipo_id = Tipos.id
 WHERE Pessoas.nome = 'Marlon Brando' AND Tipos.descricao = 'Atuação';
 
 -- c) Buscar as línguas cadastradas e quantos filmes foram rodados para cada uma.
-SELECT COUNT(filme_id) as qtde_filmes, lingua FROM Linguas
+SELECT COUNT(filme_id) as qtde_filmes, trim(lingua) FROM Linguas
 JOIN Filmes ON Linguas.filme_id = Filmes.id
 GROUP BY Linguas.lingua;
 
 -- d) Buscar as informações do ator (créditos do tipo ‘Atuação’) que mais estrelou filmes.
-SELECT COUNT(pessoa_id) AS qtd_de_filmes, Pessoas.* FROM Pessoas
+SELECT 
+	COUNT(pessoa_id) AS qtd_de_filmes, 
+	Pessoas.id, 
+	trim(Pessoas.nome), 
+	trim(Pessoas.sexo),
+	Pessoas.data_nasc 
+FROM Pessoas
 JOIN Creditos ON Creditos.pessoa_id = Pessoas.id
 JOIN Tipos    ON Creditos.tipo_id = Tipos.id
 WHERE(Tipos.descricao = 'Atuação')
@@ -39,7 +45,7 @@ WHERE(Tipos.descricao = 'Atuação' AND Generos.descricao = 'Comédia');
 -- f) Listar os créditos completos do filme ‘Pulp Fiction’. A lista deve apresentar 3 campos: o
 -- nome (Pessoa.nome) de todos os envolvidos no filme; qual o tipo do crédito
 -- (tipo.descricao); e para atores (tipo.descricao = ‘Atuação’) o nome do personagem.
-SELECT Pessoas.nome, Tipos.descricao, Creditos.personagem FROM Pessoas
+SELECT trim(Pessoas.nome), trim(Tipos.descricao), trim(Creditos.personagem) FROM Pessoas
 JOIN Creditos     ON Creditos.pessoa_id    = Pessoas.id
 JOIN Tipos        ON Creditos.tipo_id      = Tipos.id
 JOIN Filmes       ON Creditos.filme_id     = Filmes.id
